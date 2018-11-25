739599817495d8d0374c679fa1a56413b13afb3c
zep1994 <snaxbags@protonmail.com>
2018-11-25 03:16:08 -0600

    Add JS and html

---
+++ js/array-literal.js
@@ -0,0 +1,4 @@
+// Create an array and assign it
+var colors = ['white', 'black', 'custom']; // Show the first item from the
+var el = document.getElementById('colors');
+el.textContent = colors[0];/*NOTE: textContent does not work in IE8 or earlierYou can use innerHTML on line 7, but note the security issues on p228-231*/
